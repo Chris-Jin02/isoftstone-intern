7351aa4a1314b5347d41dc5185c0a4a51fe71509
chris kin <86103000+2925780078@users.noreply.github.com>
2023-06-07 23:40:37 +0800

    Add files via upload

---
+++ Day 5 work/Day 5 Study.py
@@ -0,0 +1,9 @@
+# 小组：01
+# 作者：金石
+# 创建时间：2023/6/7 23:34
+# 文件名：Day 5 Study.py
+import pandas as pd
+import numpy as np
+import matplotlib as plt
+
+df = pd.read_csv('./十大省份确诊人数.csv', encoding='gbk')
---
+++ Day 5 work/Day 5 work.py
@@ -0,0 +1,26 @@
+# 小组：01
+# 作者：金石
+# 创建时间：2023/6/7 23:33
+# 文件名：Day 5 work.py
+import pandas as pd
+import numpy as np
+import matplotlib as plt
+
+df = pd.read_csv('./新冠最新数据.csv', encoding='gbk')
+
+# 排除城市里为空的
+df = df[['province', 'confirmed']][df['city'].notnull()]
+
+# 按省份分组求和并按 confirmed 降序排列,Ture 就为升序排列
+df = df.groupby('province').sum().sort_values(by='confirmed',ascending=False)
+# print(df)
+
+df = df.iloc[0:10]
+# 简化写法
+df.insert(0, 'id', range(1, len(df)+1))
+# print(df)
+
+df = df.reset_index(drop = False) #放弃原来的索引
+df=df.set_index('id') #将 id 列设置为新的索引
+print(df)
+df.to_csv('./十大省份确诊人数.csv',encoding='gbk')
